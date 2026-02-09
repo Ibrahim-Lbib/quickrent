763855d3fb921cf55921ba89b8150379070a1d19
Ibrahim Labib <ibrahimibib840@gmail.com>
2026-02-10 01:42:15 +0200

    feat(auth): create login route and template with styling

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,1 +1,8 @@
 # login, register
+from flask import Blueprint, render_template
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/login')
+def login():
+    return render_template('auth/login.html')
