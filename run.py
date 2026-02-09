7b191fdc3e4d6f33201b7ff82537c224f42cd741
MahamatAbdelrassoul <emmanuelyohana779@gmail.com>
2026-02-09 16:57:09 +0300

    feat(app): restructure Flask app to use factory pattern and update home template

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
+def create_app():
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    @app.route('/')
+    def index():
+        return render_template('home.html')
+
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
