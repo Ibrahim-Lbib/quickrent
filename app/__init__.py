dd83de80a4485ee7fe597f42eb56b2f04204acd5
Ibrahim Labib <ibrahimibib840@gmail.com>
2026-02-09 16:45:26 +0200

    feat(app): implement index route and render home template in create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from .config import Config
 from .extensions import db, login_manager
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    @app.route('/')
+    def index():
+        return render_template('home.html')
     
-    db.init_app(app)
-    login_manager.init_app(app)
-    
-    with app.app_context():
-        db.create_all()
     
     return app
--- run.py
+++ run.py
@@ -1,16 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
-import os
+from app import create_app  
 
-def create_app():
-    app = Flask(__name__,
-                template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
-        
-
-    @app.route('/')
-    def home():
-        return render_template('home.html')
-    return app  
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
