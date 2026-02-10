62cdebf4493b423c7a840ffa66438548477a7b3c
Ibrahim Labib <ibrahimibib840@gmail.com>
2026-02-10 19:03:00 +0200

    feat(listings): implement listings route and template; add property images to listings page

--- app/routes/listings.py
+++ app/routes/listings.py
@@ -1,1 +1,8 @@
 # browse, search
+from flask import Blueprint, render_template
+
+listing_bp = Blueprint('listing', __name__)
+
+@listing_bp.route('/listings')
+def listings():
+    return render_template('listings.html')
