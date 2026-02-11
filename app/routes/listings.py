3658c75c055c22a32967eb15aba687685100a275
MahamatAbdelrassoul <emmanuelyohana779@gmail.com>
2026-02-11 19:15:54 +0300

    feat(listings): add listing_detail.html file and implement listings_detail route and add listings_detail.css file in static

--- app/routes/listings.py
+++ app/routes/listings.py
@@ -6,3 +6,7 @@ listing_bp = Blueprint('listing', __name__)
 @listing_bp.route('/listings')
 def listings():
     return render_template('listings.html')
+
+@listing_bp.route('/listings/listing_detail')
+def listings_detail():
+    return render_template('listing_detail.html')
