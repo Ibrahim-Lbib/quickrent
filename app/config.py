9bb1097fae2b760f199fee584003653ea525eb9d
Ibrahim Labib <ibrahimibib840@gmail.com>
2026-02-13 19:43:58 +0200

    feat(docs): add screenshots for home page and listings in README fix(config): revert SQLALCHEMY_DATABASE_URI to use environment variable fix(requirements): replace psycopg2-binary with flask-migrate

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:E@x3v*g8fM%ndEV@db.yshwjifylvelerylwthi.supabase.co:5432/postgres"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///quickrent.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
